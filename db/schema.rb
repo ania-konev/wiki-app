352239f740ace602cee4080a4f698daa98187641
ania-konev <akonev27@gmail.com>
2024-09-27 14:48:07 +0200

    Add navbar and styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_27_092005) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_27_100900) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_27_092005) do
     t.index ["email"], name: "index_views_on_email", unique: true
     t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "articles", "users"
 end
